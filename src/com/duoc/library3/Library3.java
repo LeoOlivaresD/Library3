77a85b985e52fe09250293553a6e643e929745f6
LeoOlivaresD <cspraword@gmail.com>
2024-06-24 01:31:06 -0400

    Se modifica metodo alquilar y devolver libros, con el proposito de tener instancias unicas solo en la clase principal. Para ello se pasa el objeto en el constructor del metodo. Ademas se borra la instanciacion de libros en el constructor dejando solo la inicializacion del metodo actualizar lista de libros disponibles.

--- src/com/duoc/library3/Library3.java
+++ src/com/duoc/library3/Library3.java
@@ -4,8 +4,13 @@ import com.duoc.library3.entities.Biblioteca;
 import com.duoc.library3.entities.OperacionesUsuario;
 import com.duoc.library3.excepciones.LibroNoEcontradoException;
 import com.duoc.library3.excepciones.LibroYaPrestadoException;
+import com.duoc.library3.inputs.InputLibros;
+import com.duoc.library3.inputs.InputsUsuarios;
+import com.duoc.library3.outputs.OutputsLibros;
+import com.duoc.library3.outputs.OutputsUsuario;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 /*
 Que tal profesor !, por aca unas pequeñas notas.
 Al menos en este proyecto me enfoque en usar todas las colecciones disponibles
@@ -19,17 +24,28 @@ Parto guardandolos en un HashMap ocupando su id como clave y luego hago una list
 de usuarios con TreeSet para almacenar solo usuarios que contengan listas de libros.
 Todo esto lo podra probar en los case del menu que mostraran datos segun cada parametro.
 De antemano muchas gracias por la revision y espero se entienda el codigo con la documentacion que realice en cada paso
-*/
+ */
 public class Library3 {
 
     static Scanner sc = new Scanner(System.in);
     static Integer opcionMenu = 0;
     static boolean salirMenu = true;
+    static String archivoCsvLibros = "libros.csv";
 
     public static void main(String[] args) throws LibroYaPrestadoException, LibroNoEcontradoException {
         //Creacion de objetos a utilizar
         OperacionesUsuario operacionesUsuario = new OperacionesUsuario();
         Biblioteca biblioteca = new Biblioteca();
+        //Manejo de archivos txt y csv
+        OutputsLibros outPutsLibros = new OutputsLibros();
+        InputLibros inputLibros = new InputLibros();
+        InputsUsuarios inputUsuarios = new InputsUsuarios();
+        OutputsUsuario outputsUsuario = new OutputsUsuario();
+        //Con este metodo estare creando un archivo csv que contenga todos los libros predeterminados
+        outPutsLibros.crearArchivoLibrosCSV(archivoCsvLibros);
+        //Aca leere el archivo csv de los libros y creare esos objetos para luego añadirlos a mi lista dentro del mismo metodo
+        inputLibros.csvAListaLibros(biblioteca, archivoCsvLibros);
+
         System.out.println("Bienvenido a la biblioteca virtual");
 
         //Menu
@@ -62,7 +78,7 @@ public class Library3 {
                         System.out.println("Antes de alquilar un libro, por favor ingrese un id de usuario "
                                 + "el cual estara asignado para el libro");
                         int idUsuarioAlquilar = sc.nextInt();
-                        biblioteca.alquilarLibro(operacionesUsuario, idUsuarioAlquilar);
+                        biblioteca.alquilarLibro(outputsUsuario, operacionesUsuario, idUsuarioAlquilar);
                     } catch (InputMismatchException e) {
                         System.out.println("Lo sentimos, debe ingresar un numero id valido");
                         sc.nextLine(); //Limpio el buffer de scanner
@@ -78,7 +94,7 @@ public class Library3 {
                         operacionesUsuario.mostrarLibrosAlquilados(idUsuarioDevolver);//Muestro los lbros con su id respectivo alquilados por el usuario
                         System.out.println("Ahora ingrese id del libro a devolver");
                         int idLibroDevolver = sc.nextInt();
-                        biblioteca.devolverLibro(operacionesUsuario, idUsuarioDevolver, idLibroDevolver);
+                        biblioteca.devolverLibro(outputsUsuario, operacionesUsuario, idUsuarioDevolver, idLibroDevolver);
                     } catch (InputMismatchException e) {
                         System.out.println("Lo sentimos, debe ingresar un numero id valido para usuario o libro");
                         sc.nextLine(); //Limpio el buffer de scanner
