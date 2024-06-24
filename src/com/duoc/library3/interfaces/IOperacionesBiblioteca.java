0bb179c92d2c3ef6706f58dc7fd7557d9325888a
LeoOlivaresD <cspraword@gmail.com>
2024-06-24 01:32:33 -0400

    Se modifica constructor de los metodos alquilar y devolver libros para mantener una unica instancia de las clases operaciones usuario y outputs usuario

--- src/com/duoc/library3/interfaces/IOperacionesBiblioteca.java
+++ src/com/duoc/library3/interfaces/IOperacionesBiblioteca.java
@@ -1,11 +1,12 @@
 package com.duoc.library3.interfaces;
 
 import com.duoc.library3.entities.OperacionesUsuario;
+import com.duoc.library3.outputs.OutputsUsuario;
 
 public interface IOperacionesBiblioteca {
     public void mostrarListaLibros();
-    public void alquilarLibro(OperacionesUsuario operacionesUsuario, int idUsuario);
+    public void alquilarLibro(OutputsUsuario fw ,OperacionesUsuario operacionesUsuario, int idUsuario);
     public void actualizarListalibrosDisponibles();
     public void mostrarListaConLibrosDisponibles();
-    public void devolverLibro(OperacionesUsuario operacionesUsuario, int idUsuario, int idLibro);
+    public void devolverLibro(OutputsUsuario fw ,OperacionesUsuario operacionesUsuario, int idUsuario, int idLibro);
 }
