bdbd5ca0a390c09e01d3506f783d3feb10683809
LeoOlivaresD <cspraword@gmail.com>
2024-06-24 01:28:34 -0400

    Se modifica formato de guardado del objeto , ya que se estaba guardando primero el id y al momento de llamar al metodo para leer el archivo csv este trataba de hacer la conversion de un boolean cuando en realidad era un integer ocacionando un error

--- src/com/duoc/library3/outputs/OutputsLibros.java
+++ src/com/duoc/library3/outputs/OutputsLibros.java
@@ -12,17 +12,17 @@ public class OutputsLibros {
         File archivo = new File(nombreArchivo);
         if (!archivo.exists()) {
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo))) {
-                bw.write("idLibro,titulo,autor,disponibilidad");
+                bw.write("titulo,autor,disponibilidad,idLibro");
                 bw.newLine();
-                bw.write("1,El alquimista,Paulo Coelho,true");
+                bw.write("El alquimista,Paulo Coelho,true,1");
                 bw.newLine();
-                bw.write("2,La odisea,Homero,true");
+                bw.write("La odisea,Homero,true,2");
                 bw.newLine();
-                bw.write("3,Romeo y Julieta,William Shakespeare,true");
+                bw.write("Romeo y Julieta,William Shakespeare,true,3");
                 bw.newLine();
-                bw.write("4,Don Quijote,Miguel de Cervantes,true");
+                bw.write("Don Quijote,Miguel de Cervantes,true,4");
                 bw.newLine();
-                bw.write("5,The Hobbit,J.R.R. Tolkien,true");
+                bw.write("The Hobbit,J.R.R. Tolkien,true,5");
                 bw.newLine();
             } catch (IOException e) {
                 e.printStackTrace();
